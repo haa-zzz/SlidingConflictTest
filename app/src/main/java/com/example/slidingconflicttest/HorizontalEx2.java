f5c3c31feaaf4d1534ab7537f2a23a2d9c177bb1
T20210145 <2101297547@qq.com>
2021-03-10 21:25:28 +0800

    简单的修改

--- app/src/main/java/com/example/slidingconflicttest/HorizontalEx2.java
+++ app/src/main/java/com/example/slidingconflicttest/HorizontalEx2.java
@@ -1,5 +1,6 @@
 package com.example.slidingconflicttest;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -10,10 +11,13 @@ import android.view.ViewGroup;
 import android.widget.Scroller;
 
 public class HorizontalEx2 extends ViewGroup {
+    //分别记录上次滑动的坐标
     private int lastX,lastY;
+    //分别记录上次滑动的坐标（onInterceptTouchEvent）
+    private int mLastXIntercept,mLastYIntercept;
     private int childIndex;
-    private Scroller mScroller;
-    private VelocityTracker mVelocityTracker;
+    private Scroller mScroller; //用于弹性滑动
+    private VelocityTracker mVelocityTracker;   //速度追踪
 
     public HorizontalEx2(Context context) {
         super(context);
@@ -44,19 +48,22 @@ public class HorizontalEx2 extends ViewGroup {
         int childCount = getChildCount();
         measureChildren(widthMeasureSpec, heightMeasureSpec);
 
-        if (childCount == 0) {
+        if (childCount == 0) {              //如果没有子元素
             setMeasuredDimension(0, 0);
-        } else if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+        } else if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {  //宽/高是否采取了wrap_content
             height = getChildAt(0).getMeasuredHeight();
             width = childCount * getChildAt(0).getMeasuredWidth();
             setMeasuredDimension(width, height);
-        } else if (widthMode == MeasureSpec.AT_MOST) {
+        } else if (widthMode == MeasureSpec.AT_MOST) {                          //宽是否采取了wrap_content，是的话 宽是所有子元素之和
             width = childCount * getChildAt(0).getMeasuredWidth();
             setMeasuredDimension(width, height);
-        } else {
+        } else if(heightMode == MeasureSpec.AT_MOST){                            //高是否采取wrap_content， 是的话 高是第一个元素的高
             height = getChildAt(0).getMeasuredHeight();
+            setMeasuredDimension(width, height);
+        }else{
             setMeasuredDimension(width, height);
         }
+
     }
 
     @Override
@@ -64,30 +71,31 @@ public class HorizontalEx2 extends ViewGroup {
         int leftOffset = 0;
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
-            child.layout(l + leftOffset, t, r + leftOffset, b);
-            leftOffset += child.getMeasuredWidth();
+            if(child.getVisibility() != View.GONE){
+                child.layout(l + leftOffset, t, r + leftOffset, b);
+                leftOffset += child.getMeasuredWidth();
+            }
+
         }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if(ev.getAction() == MotionEvent.ACTION_DOWN){
-            if(!mScroller.isFinished()){
-                mScroller.abortAnimation();
-                return true;
-            }
+        int action = ev.getAction();
+        if(action == MotionEvent.ACTION_DOWN){
             return false;
-        }
-        else{
+        }else {
             return true;
         }
-    }
-    private boolean isFirstTouch = true;
 
+    }
+    //private boolean isFirstTouch = true;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        int x = (int)event.getX();
+        int x = (int)event.getX();  //相对当前View左上角的x和y坐标
         int y = (int)event.getY();
 
         mVelocityTracker.addMovement(event);
@@ -99,16 +107,19 @@ public class HorizontalEx2 extends ViewGroup {
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
+                /*
                 if(isFirstTouch){
                     isFirstTouch = false;      //在开始滑动时更新lastX,lastY
                     lastX = x;
                     lastY = y;
                 }
+
+                 */
                 final int deltaX = x-lastX;
                 scrollBy(-deltaX,0);
                 break;
             case MotionEvent.ACTION_UP:
-                isFirstTouch = true;
+                //isFirstTouch = true;
                 int scrollX = getScrollX();
                 mVelocityTracker.computeCurrentVelocity(1000, configuration.getScaledMaximumFlingVelocity());
                 float mVelocityX = mVelocityTracker.getXVelocity();     //水平速度
@@ -122,10 +133,9 @@ public class HorizontalEx2 extends ViewGroup {
                 smoothScrollBy(childIndex*getChildAt(0).getWidth()-scrollX,0);
                 mVelocityTracker.clear();
                 break;
-
         }
         lastX = x;
-        lastY  = y;
+        lastY = y;
         return true;
     }
     private void smoothScrollBy(int dx, int dy) {
